c7f8bde88d86940d7ca75f746c2a279edba7359f
trang.nguyen <trang.nguyen@LGVN14343.logigear.com>
2018-02-06 16:10:37 +0700

    trang add tc03

--- TestCases/TestModules/LoginTM.java
+++ TestCases/TestModules/LoginTM.java
@@ -29,6 +29,20 @@ public class LoginTM extends TestBase.TestBase  {
 	  Assert.assertEquals(ActualMessage, ExpectedMessage);
 
   }
-  
+   
+  public void TC003_login_page_incorrect_credentials() {
+	  System.out.println("DA_LOGIN_TC002 - Verify that user fails to login specific repository successfully via Dashboard login page with incorrect credentials.");
+	  LoginPage loginpage = new LoginPage (driver);
+	  loginpage.Open();
+	  loginpage.Login("abc","123",Constant.DefaultRepo);
+	  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS); 
+	  String ActualMessage = loginpage.GetAlertMessage();
+	  String ExpectedMessage = "Username or password is invalid";
+	  System.out.print(ExpectedMessage); 
+	  System.out.print (ActualMessage);
+	 
+	  Assert.assertEquals(ActualMessage, ExpectedMessage);
+
+  }
   }
 
