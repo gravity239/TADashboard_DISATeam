cdf1484d86e2271dc1e0e25dde8a80bbd13448bc
thong.nguyen <thong.nguyen@lgvn14196.logigear.com>
2018-02-07 15:46:05 +0700

    Merge TestBase

--- TestCases/TestBase/TestBase.java
+++ TestCases/TestBase/TestBase.java
@@ -11,27 +11,7 @@ import org.testng.asserts.SoftAssert;
 public class TestBase {
 
 	String driverPath = "./Executables/chromedriver.exe";
-<<<<<<< HEAD
     public WebDriver driver;	 
-<<<<<<< HEAD
-    SoftAssert softAssert = new SoftAssert();
-    
-    @BeforeMethod
-    public void beforeMethod_initialize() 
-    {
-    	if(driver.toString().contains("null"))
-    	{
-    		 System.setProperty("webdriver.chrome.driver", driverPath);
-    	     driver = new ChromeDriver();
-    	     driver.manage().window().maximize();
-    	}
-    }
-    
-    @AfterMethod 
-    public void afterMethod_cleanup()
-     {    	
-    	driver.quit();    	
-=======
     protected SoftAssert softAssert = new SoftAssert();
     
     @BeforeMethod
@@ -48,10 +28,9 @@ public class TestBase {
     @AfterMethod 
     public void afterMethod_cleanup()
      {    	
-    	driver.close();    	
->>>>>>> branch 'master' of https://github.com/gravity239/TADashboard_DISATeam.git
-	 }
-    
+    	driver.quit();    	
+     }
+
 	@BeforeTest
     public void beforeTest_initialize() 
     {
@@ -63,36 +42,8 @@ public class TestBase {
 	 @AfterTest 
 	 public void afterTest_cleanup()
 	 {    	
-	    driver.quit();    	
-=======
-	public WebDriver driver;
-	SoftAssert softAssert = new SoftAssert();
+	    driver.quit();  
+	 }
 
-	@BeforeMethod
-	public void beforeMethod_initialize() {
-		if (driver.toString().contains("null")) {
-			System.setProperty("webdriver.chrome.driver", driverPath);
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-		}
->>>>>>> branch 'master' of https://github.com/gravity239/TADashboard_DISATeam.git
-	}
-
-	@AfterMethod
-	public void afterMethod_cleanup() {
-		driver.quit();
-	}
-
-	@BeforeTest
-	public void beforeTest_initialize() {
-		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-	}
-
-	@AfterTest
-	public void afterTest_cleanup() {
-		driver.quit();
-	}
 
 }
