e605db0998921271fcf54d8feea152fe44ed3276
trang.nguyen <trang.nguyen@LGVN14343.logigear.com>
2018-02-07 14:08:32 +0700

    temp

--- Common/Constant/Constant.java
+++ Common/Constant/Constant.java
@@ -2,7 +2,11 @@ package Constant;
 
 public class Constant 
 	{
+<<<<<<< HEAD
 		public static final String HomePageURL = "http://localhost:54000/TADashboard/login.jsp";
+=======
+		public static final String HomePageURL = "http://lgvn14954:54000";
+>>>>>>> branch 'master' of https://github.com/gravity239/TADashboard_DISATeam.git
 		public static final String Username = "administrator";
 		public static final String Password = "";
 		public static final String OtherUsername = "test";
