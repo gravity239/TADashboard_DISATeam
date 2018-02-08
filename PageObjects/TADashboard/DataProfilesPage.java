9136e5643be6a3c55c8ce6c9e46045e3bf98cc57
thong.nguyen <thong.nguyen@lgvn14196.logigear.com>
2018-02-08 09:35:58 +0700

    Add DataProfilesPage class

---
+++ PageObjects/TADashboard/DataProfilesPage.java
@@ -0,0 +1,15 @@
+package TADashboard;
+
+import org.openqa.selenium.WebDriver;
+
+public class DataProfilesPage extends GeneralPage{
+	
+	private WebDriver _driverDPPage;
+	
+	public DataProfilesPage(WebDriver driver) {
+		super(driver);
+		this._driverDPPage = driver;
+	}
+	
+	
+}
