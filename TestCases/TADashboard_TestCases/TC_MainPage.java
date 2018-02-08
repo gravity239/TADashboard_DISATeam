17ea8d880de3c013a66d9078da48dbd12b4b8e6c
trang.nguyen <trang.nguyen@LGVN14343.logigear.com>
2018-02-08 17:30:26 +0700

    add TC17

--- TestCases/TADashboard_TestCases/TC_MainPage.java
+++ TestCases/TADashboard_TestCases/TC_MainPage.java
@@ -34,18 +34,29 @@ public class TC_MainPage extends TestBase {
 		mainPage.deletePage(pageName);
 		softAssert.assertAll();
 	}
-  	
+
 	@Test
-	public void TC017_DA_MP()
-	{
-		System.out.println("Verify non \"Public\" pages can only be accessed and visible to their creators with condition that all parent pages above it are \"Public\"");
+	public void TC017_DA_MP() {
+		System.out.println("Verify \"Public\" pages can be visible and accessed by all users of working repository");
 		String pageName = Utilities.uniquePageName("Page");
+		// 1. Navigate to Dashboard login page. Login with valid account
+		// 2. Go to Global Setting -> Add page. Enter Page Name field
 		LoginPage loginPage = new LoginPage(driver);
 		MainPage mainPage = loginPage.open().login(Constant.Username, Constant.Password, Constant.DefaultRepo);
-		mainPage.addPage(pageName, "", 0, "", false);
+		mainPage.addPage(pageName, "", 0, "", true);
+		// VP Check Page added successfully
 		mainPage.isPageExisted(pageName);
-		
-	
+		mainPage.logout();
+		// Login with other accounts
+		loginPage.login(Constant.OtherUsername, Constant.OtherPassword, Constant.DefaultRepo);
+
+		softAssert.assertEquals(mainPage.isPageExisted(pageName), true);
+		// Post-condition: Delete newly added page
+		mainPage.logout();
+		loginPage.login(Constant.Username, Constant.Password, Constant.DefaultRepo);
+		mainPage.deletePage(pageName);
+		softAssert.assertAll();
+
 	}
-	
+
 }
