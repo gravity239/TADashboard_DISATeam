cdb7dcda22a4756ad68b518ff11b0cc26ac49a66
thong.nguyen <thong.nguyen@lgvn14196.logigear.com>
2018-02-09 17:50:26 +0700

    No change

--- Common/Constant/Constant.java
+++ Common/Constant/Constant.java
@@ -1,20 +1,19 @@
 package Constant;
 
-public class Constant 
-	{
-		public static final String HomePageURL = "http://lgvn14954:54000/TADashboard/login.jsp";
-		public static final String Username = "administrator";
-		public static final String Password = "";
-		public static final String OtherUsername = "test";
-		public static final String OtherUsername1 = "test1";
-		public static final String OtherPassword = "TEST";
-		public static final String SpecialUsername = "test@!";
-		public static final String SpecialPassword = "test@#";
-		public static final String DefaultRepo = "SampleRepository";
-		public static final String AdditionalRepo = "TestRepository";
-		public static final long TimeOut = 20;
-		public static final long SleepTime = 3;
-		public static final long ShortTime = 5;
-		public static final long LongTime = 10;		
-	}
+public class Constant {
+	public static final String HomePageURL = "http://lgvn14954:54000/TADashboard/login.jsp";
+	public static final String Username = "administrator";
+	public static final String Password = "";
+	public static final String OtherUsername = "test";
+	public static final String OtherUsername1 = "test1";
+	public static final String OtherPassword = "TEST";
+	public static final String SpecialUsername = "test@!";
+	public static final String SpecialPassword = "test@#";
+	public static final String DefaultRepo = "SampleRepository";
+	public static final String AdditionalRepo = "TestRepository";
+	public static final long TimeOut = 20;
+	public static final long SleepTime = 3;
+	public static final long ShortTime = 5;
+	public static final long LongTime = 10;
 
+}
